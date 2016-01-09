d000ed254cfbcaf4a1e84d05244d8e2deac8d8ff
Rafał Tymiński <tyminski.rafal@gmail.com>
2016-01-09 23:40:51 +0100

    Fixed notification update. Reduce and increase buttons unmute volume also.

--- soundctl.sh
+++ soundctl.sh
@@ -19,11 +19,13 @@ volume_notify () {
     else
         volume_icon="notification-audio-volume-high"
     fi
-    notify-send "Volume" -u critical -t 2000 -i $volume_icon -h int:value:$volume -h string:x-canonical-private-synchronous:
+#    gdbus call --session --dest org.freedesktop.Notifications --object-path "/org/freedesktop/Notifications" --method org.freedesktop.Notifications.Notify soundctl 0 $volume_icon "Volume" "" [] "{\"urgency\": <byte 2>, \"value\": <int32 $volume>, \"x-canonical-private-synchronous\": <string \"0\">}" "int32 2000"
+    notify-send "Volume" -u critical -t 2000 -i $volume_icon -h int:value:$volume -h string:x-canonical-private-synchronous:0
 }
 
 mute_notify () {
-    notify-send "Volume muted" -u critical -t 2000 -i notification-audio-volume-muted -h int:value:0 -h string:x-canonical-private-synchronous:
+#    gdbus call --session --dest org.freedesktop.Notifications --object-path "/org/freedesktop/Notifications" --method org.freedesktop.Notifications.Notify soundctl 0 notification-audio-volume-muted "Volume muted" "" [] "{\"value\": <int32 0>}" "int32 2000"
+    notify-send "Volume muted" -u critical -t 2000 -i notification-audio-volume-muted -h int:value:0 -h string:x-canonical-private-synchronous:0
 }
 
 while [ "$1" != "" ]; do
@@ -59,9 +61,17 @@ if [ "$cmd" = "mute" ]; then
 fi
 
 if [ "$cmd" = "up" ]; then
+    mute=`pactl list sinks | perl -000ne 'if(/#1/){/(Mute:.*)/; print "$1\n"}' | cut -c 7-`
+    if [ "$mute" = "yes" ]; then
+        pactl set-sink-mute $default_sink toggle
+    fi
     pactl set-sink-volume $default_sink +$step%
     volume_notify
 elif [ "$cmd" = "down" ]; then
+    mute=`pactl list sinks | perl -000ne 'if(/#1/){/(Mute:.*)/; print "$1\n"}' | cut -c 7-`
+    if [ "$mute" = "yes" ]; then
+        pactl set-sink-mute $default_sink toggle
+    fi
     pactl set-sink-volume $default_sink -$step%
     volume_notify
 fi
